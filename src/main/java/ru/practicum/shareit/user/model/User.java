32933902bd33ef6f9894f0ae2ea05f98ced0a74a
Andreas <ageling@yandex.ru>
2022-08-30 14:22:09 +0200

    init: add-bookings

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -3,12 +3,35 @@ package ru.practicum.shareit.user.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+//import org.hibernate.annotations.Entity;
+//import org.hibernate.annotations.Table;
+import javax.persistence.*;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Builder
+@Entity
+@Table(name = "users")
 public class User {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+////    @Column(name = "first_name", nullable = false)
+//    private String firstName;
+//
+////    @Column(name = "last_name")
+//    private String lastName;
+//    private String email;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
 }
+
+
